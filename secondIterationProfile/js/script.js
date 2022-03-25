fda5bc7804e41ef7ea89e46ec78fc5b246f203d6
rogerb5 <rbenjume@ewu.edu>
2022-03-25 07:44:05 -0700

    SecondIterationProfile

---
+++ secondIterationProfile/js/script.js
@@ -0,0 +1,74 @@
+const scrollContainer = document.querySelector('.scroll-container');
+const projectImages = Array.from(document.querySelectorAll('img'));
+const navBar = document.querySelector('.nav-bar');
+const aboutS = document.querySelector('.s-about');
+
+const projectsSection = document.querySelector('.projects');
+const projectTitle = document.querySelector('.proj-title');
+var isScrolling = false;
+
+let current = 0;
+let target = 0;
+let ease = 0.095;
+
+function setDocumentHeight() {
+    document.body.style.height = `${scrollContainer.getBoundingClientRect().height}px`;
+}
+
+function lerp(start, end, t) {
+    return start * (1 - t) + end * t;
+}
+
+function animate() {
+    target = window.scrollY;
+    current = lerp(current, target, ease);
+    scrollContainer.style.transform = `translate3d(0, ${-current}px, 0)`;
+    animateProjects();
+    requestAnimationFrame(animate);
+}
+
+function animateProjects() {
+    for (let i = 0; i < projectImages.length; i++) {
+        let { top } = projectImages[i].parentElement.getBoundingClientRect();
+        if (i % 2 == 0) {
+            projectImages[i].style.transform = `translate3d(0, ${top * 0.9}px, 0) rotate(${top * 0.04}deg)`;
+        } else {
+            projectImages[i].style.transform = `translate3d(0, ${top * 0.1}px, 0) rotate(${top * 0.04}deg)`;
+        }
+    }
+}
+
+function throttle(animate, time) {
+    if (!isScrolling) {
+        isScrolling = true;
+        setTimeout(() => {
+            animate();
+            isScrolling = false;
+        }, time)
+    }
+    return;
+}
+
+window.addEventListener('scroll', function () {
+    throttle(animate, 250);
+})
+
+setDocumentHeight();
+
+const overlayIntro = document.querySelector('.overlay-intro');
+const scrollBtn = document.querySelector('.nav-link');
+const scrollBtn2 = document.querySelector('.nav-link2');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        overlayIntro.querySelector('h1').style.color = '#171717';
+        setTimeout(() => {
+            overlayIntro.querySelector('h1').style.opacity = '.7';
+        }, 600)
+    }, 1000)
+    setTimeout(() => {
+        overlayIntro.style.opacity = 0;
+    }, 3000)
+})
+
